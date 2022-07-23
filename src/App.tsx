84c55081b56dd35afe37e20c3926836530f6cd9f
Hendrik Mans <hendrik@mans.de>
2022-07-23 20:01:12 +0200

    Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import { Environment } from "@react-three/drei"
+import { Environment, PerspectiveCamera } from "@react-three/drei"
 import { Canvas } from "@react-three/fiber"
 import { sRGBEncoding } from "three"
-import Gameplay from "./scenes/Gameplay"
+import { Gameplay } from "./scenes/Gameplay"
 
 function App() {
   return (
     <Canvas flat dpr={1} gl={{ outputEncoding: sRGBEncoding }}>
       <color attach="background" args={["#444"]} />
       <Environment preset="sunset" />
+      <PerspectiveCamera position={[0, 0, 20]} makeDefault />
 
       <Gameplay />
     </Canvas>
