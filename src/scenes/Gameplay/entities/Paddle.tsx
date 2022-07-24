fed7b2dcacecae31a4f3f63dfcd908b2fdcfd123
Hendrik Mans <hendrik@mans.de>
2022-07-24 22:03:28 +0200

    Cleanup

--- src/scenes/Gameplay/entities/Paddle.tsx
+++ src/scenes/Gameplay/entities/Paddle.tsx
@@ -1,6 +1,5 @@
 import { ReactNode } from "react"
 import { ColorRepresentation } from "three"
-import { Rect } from "../../../lib/Rect"
 import { ECS } from "../state"
 
 export type PaddleProps = {
@@ -12,7 +11,6 @@ export type PaddleProps = {
 export const Paddle = ({ position, color, children }: PaddleProps) => (
   <ECS.Entity>
     <ECS.Component name="isPaddle" data={true} />
-    <ECS.Component name="bounds" data={Rect(position, -3.5, position, 3.5)} />
     <ECS.Component name="velocity">
       <vector2 />
     </ECS.Component>
