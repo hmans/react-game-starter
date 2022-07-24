be702ce18458701fdf4707916519a6b1cdb13b2a
Hendrik Mans <hendrik@mans.de>
2022-07-24 22:05:25 +0200

    Cleanup

--- src/scenes/Gameplay/systems/ballSystem.ts
+++ src/scenes/Gameplay/systems/ballSystem.ts
@@ -9,26 +9,25 @@ export function ballSystem() {
     If an area component is present, check if the entity has left the
     area; if so, reverse its velocity.
     */
-    if (bounds) {
-      /* Vertical collision */
-      if (transform.position.y < bounds.y) {
-        velocity.y = -velocity.y
-        transform.position.y = bounds.y
-      } else if (transform.position.y > bounds.y + bounds.height) {
-        velocity.y = -velocity.y
-        transform.position.y = bounds.y + bounds.height
-      }
 
-      /* Horizontal collision with wall -- score! */
-      if (transform.position.x < bounds.x) {
-        velocity.x = -velocity.x
-        transform.position.x = bounds.x
-        increaseEnemyScore()
-      } else if (transform.position.x > bounds.x + bounds.width) {
-        velocity.x = -velocity.x
-        transform.position.x = bounds.x + bounds.width
-        increasePlayerScore()
-      }
+    /* Vertical collision */
+    if (transform.position.y < bounds.y) {
+      velocity.y = -velocity.y
+      transform.position.y = bounds.y
+    } else if (transform.position.y > bounds.y + bounds.height) {
+      velocity.y = -velocity.y
+      transform.position.y = bounds.y + bounds.height
+    }
+
+    /* Horizontal collision with wall -- score! */
+    if (transform.position.x < bounds.x) {
+      velocity.x = -velocity.x
+      transform.position.x = bounds.x
+      increaseEnemyScore()
+    } else if (transform.position.x > bounds.x + bounds.width) {
+      velocity.x = -velocity.x
+      transform.position.x = bounds.x + bounds.width
+      increasePlayerScore()
     }
   }
 }
