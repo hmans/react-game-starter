f22237f400f490221335e3a4850792ce4d094afb
Hendrik Mans <hendrik@mans.de>
2022-07-24 22:36:30 +0200

    Update paddle collisions

--- src/scenes/Gameplay/systems/ballSystem.ts
+++ src/scenes/Gameplay/systems/ballSystem.ts
@@ -1,19 +1,24 @@
+import { Box2, Vector2 } from "three"
 import { ballRadius, paddleHeight, paddleWidth } from "../configuration"
 import { ECS, increaseEnemyScore, increasePlayerScore } from "../state"
 
 const { entities: balls } = ECS.world.archetype("ball")
 const { entities: paddles } = ECS.world.archetype("isPaddle")
 
+const ballDimensions = new Box2(
+  new Vector2(-ballRadius, -ballRadius),
+  new Vector2(+ballRadius, +ballRadius)
+)
+
+const tmpBox2 = new Box2()
+
 export function ballSystem() {
   for (const { transform, velocity, ball } of balls) {
     const { bounds } = ball
 
-    // const ballAABB = AABB(
-    //   transform.position.x - ballRadius,
-    //   transform.position.y - ballRadius,
-    //   ballRadius * 2,
-    //   ballRadius * 2
-    // )
+    const ballAABB = tmpBox2
+      .copy(ballDimensions)
+      .translate(new Vector2(transform.position.x, transform.position.y))
 
     /* Vertical collision */
     if (transform.position.y < bounds.min.y) {
@@ -36,17 +41,25 @@ export function ballSystem() {
     }
 
     /* Check paddle collisions */
-    // for (const paddle of paddles) {
-    //   const paddleAABB = AABB(
-    //     paddle.transform.position.x - paddleWidth / 2,
-    //     paddle.transform.position.y - paddleHeight / 2,
-    //     paddleWidth,
-    //     paddleHeight
-    //   )
+    for (const paddle of paddles) {
+      const paddleBox = new Box2(
+        new Vector2(
+          paddle.transform.position.x - paddleWidth / 2,
+          paddle.transform.position.y - paddleHeight / 2
+        ),
+        new Vector2(
+          paddle.transform.position.x + paddleWidth / 2,
+          paddle.transform.position.y + paddleHeight / 2
+        )
+      )
 
-    //   if (checkAABB(ballAABB, paddleAABB)) {
-    //     velocity.x = -velocity.x
-    //   }
-    // }
+      if (ballAABB.intersectsBox(paddleBox)) {
+        velocity.x < 0
+          ? (transform.position.x = paddleBox.max.x + ballRadius)
+          : (transform.position.x = paddleBox.min.x - ballRadius)
+
+        velocity.x *= -1
+      }
+    }
   }
 }
