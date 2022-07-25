4faf99c90672c79fc44e04ea7218a8e4cbc7c231
Hendrik Mans <hendrik@mans.de>
2022-07-25 12:23:31 +0200

    Add r3f-perf

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Environment, PerspectiveCamera } from "@react-three/drei"
 import { Canvas } from "@react-three/fiber"
+import { Perf } from "r3f-perf"
 import { PostProcessing } from "./lib/PostProcessing"
 import { Gameplay } from "./scenes/Gameplay"
 
@@ -10,6 +11,7 @@ function App() {
       <Environment preset="sunset" />
       <PerspectiveCamera position={[0, 0, 20]} makeDefault />
       <PostProcessing />
+      <Perf position="bottom-right" deepAnalyze matrixUpdate />
 
       <Gameplay />
     </Canvas>
