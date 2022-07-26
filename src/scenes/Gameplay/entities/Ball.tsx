9f4915f755d78150c6d8a24e9ab8ba72c0efb434
Hendrik Mans <hendrik@mans.de>
2022-07-26 15:13:03 +0200

    Ball

--- src/scenes/Gameplay/entities/Ball.tsx
+++ src/scenes/Gameplay/entities/Ball.tsx
@@ -1,6 +1,6 @@
 import { Animate, AnimationFunction } from "../../../lib/Animate"
 import { ballRadius } from "../configuration"
-import { setGameObject } from "../state"
+import { GameObject } from "../state"
 
 const rotate: AnimationFunction = (dt, { rotation }) => {
   rotation.x += 0.3 * dt
@@ -8,12 +8,12 @@ const rotate: AnimationFunction = (dt, { rotation }) => {
 }
 
 export const Ball = () => (
-  <group ref={setGameObject("ball")}>
+  <GameObject name="ball">
     <Animate update={rotate}>
       <mesh>
         <dodecahedronGeometry args={[ballRadius]} />
         <meshStandardMaterial color="white" metalness={0.2} roughness={0.1} />
       </mesh>
     </Animate>
-  </group>
+  </GameObject>
 )
