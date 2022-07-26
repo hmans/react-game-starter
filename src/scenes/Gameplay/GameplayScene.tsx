9f751f8b4b2d188526acdc07b5f464dd6517b6fb
Hendrik Mans <hendrik@mans.de>
2022-07-26 20:41:37 +0200

    Import timeline components

--- src/scenes/Gameplay/GameplayScene.tsx
+++ src/scenes/Gameplay/GameplayScene.tsx
@@ -1,46 +1,48 @@
-import { useLayoutEffect } from "react"
 import { Animate, AnimationFunction } from "../../lib/Animate"
+import { Effect } from "../../lib/Effect"
 import { Keypress } from "../../lib/Keypress"
+import { Delay } from "../../lib/timeline-composer"
 import { returnToTitle } from "../../state"
 import Court from "./Court"
-import { Effect } from "../../lib/Effect"
 import { Ball, Enemy, Player } from "./entities"
 import { ScoreHUD } from "./ScoreHUD"
 import { initializeGameplay, setGameObject, startRound, store } from "./state"
 import { Systems } from "./systems/Systems"
 
-export const GameplayScene = () => {
-  const followBall: AnimationFunction = (dt, object) => {
-    /* We can't afford to do this reactively here, will tweak later... */
-    const ball = store.state.ball
+const followBall: AnimationFunction = (dt, object) => {
+  /* We can't afford to do this reactively here, will tweak later... */
+  const ball = store.state.ball
 
-    if (!ball) return
-    object.rotation.x = ball.position.y / -60
-    object.rotation.y = ball.position.x / 120
-  }
-
-  return (
-    <group>
-      <Keypress code="Escape" onPress={returnToTitle} />
-      <Effect callback={initializeGameplay} />
-      <Effect callback={startRound} />
-
-      <Animate update={followBall}>
-        <Court position-z={-0.5} />
-
-        <ScoreHUD position={[0, 4, 1]} />
-
-        <Player />
-        <Enemy />
-        <Ball />
-
-        <CameraTarget />
-      </Animate>
-
-      <Systems />
-    </group>
-  )
+  if (!ball) return
+  object.rotation.x = ball.position.y / -60
+  object.rotation.y = ball.position.x / 120
 }
+
+export const GameplayScene = () => (
+  <group>
+    <Keypress code="Escape" onPress={returnToTitle} />
+
+    <Effect callback={initializeGameplay} />
+
+    <Delay seconds={0.75}>
+      <Effect callback={startRound} />
+    </Delay>
+
+    <Animate update={followBall}>
+      <Court position-z={-0.5} />
+
+      <ScoreHUD position={[0, 4, 1]} />
+
+      <Player />
+      <Enemy />
+      <Ball />
+
+      <CameraTarget />
+    </Animate>
+
+    <Systems />
+  </group>
+)
 
 const CameraTarget = ({ debug = false }) => (
   <group ref={setGameObject("cameraTarget")}>
