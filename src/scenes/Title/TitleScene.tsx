9e227e486045f3abf6343db6576e2a86c0bf515d
Hendrik Mans <hendrik@mans.de>
2022-07-26 20:20:17 +0200

    Keypress and useKeypress

--- src/scenes/Title/TitleScene.tsx
+++ src/scenes/Title/TitleScene.tsx
@@ -1,19 +1,12 @@
 import { Text } from "@react-three/drei"
-import { useFrame } from "@react-three/fiber"
-import { controller } from "../../input/controller"
+import { Keypress } from "../../lib/Keypress"
 import { enterGameplay } from "../../state/macroState"
 
-export const TitleScene = () => {
-  useFrame(() => {
-    /* TODO: ControlFreak really needs actions/interactions! */
-    if (controller.controls.fire.value > 0) {
-      enterGameplay()
-    }
-  })
-
-  return (
+export const TitleScene = () => (
+  <>
+    <Keypress code="Space" onPress={enterGameplay} />
     <Text position={[0, 0, 10]} fontSize={1} textAlign="center">
       {"PRESS SPACE\nTO PONG"}
     </Text>
-  )
-}
+  </>
+)
