aaef0bb9717b5bc1a601b153c1bd4080b3ad9cf3
Hendrik Mans <hendrik@mans.de>
2022-07-27 00:37:36 +0200

    Animate forwardRef

--- src/lib/Animate.tsx
+++ src/lib/Animate.tsx
@@ -1,5 +1,12 @@
 import { RootState, useFrame } from "@react-three/fiber"
-import { FC, ReactNode, useEffect, useRef } from "react"
+import {
+  FC,
+  forwardRef,
+  ReactNode,
+  useEffect,
+  useImperativeHandle,
+  useRef
+} from "react"
 import { Group, Object3D } from "three"
 
 export type AnimationFunction = <T extends Object3D>(
@@ -8,21 +15,27 @@ export type AnimationFunction = <T extends Object3D>(
   r3fstate: RootState
 ) => void
 
-export const Animate: FC<{
+export type AnimateProps = {
   children?: ReactNode
   update?: AnimationFunction
   init?: (object: Group) => void
-}> = ({ children, update, init }) => {
-  const ref = useRef<Group>(null!)
-
-  useEffect(() => {
-    if (!ref.current) return
-    init?.(ref.current)
-  }, [])
-
-  useFrame((state, dt) => {
-    update?.(dt, ref.current!, state)
-  })
-
-  return <group ref={ref}>{children}</group>
 }
+
+export const Animate = forwardRef<Group, AnimateProps>(
+  ({ children, update, init }, ref) => {
+    const group = useRef<Group>(null!)
+
+    useEffect(() => {
+      if (!group.current) return
+      init?.(group.current)
+    }, [])
+
+    useFrame((state, dt) => {
+      update?.(dt, group.current!, state)
+    })
+
+    useImperativeHandle(ref, () => group.current)
+
+    return <group ref={group}>{children}</group>
+  }
+)
