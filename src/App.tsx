eedc51b0dccd490c7d64239ab37ae03411e222e2
Hendrik Mans <hendrik@mans.de>
2022-07-26 10:11:25 +0200

    Add a simple React component render logger

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
 import { Environment } from "@react-three/drei"
 import { Canvas } from "@react-three/fiber"
 import { Camera } from "./lib/camera-composer"
+import { ComponentRenderLogger } from "./lib/ComponentRenderLogger"
 import { PostProcessing } from "./lib/PostProcessing"
 import { Gameplay } from "./scenes/Gameplay"
 
 function App() {
   return (
-    <Canvas flat dpr={1}>
-      <color attach="background" args={["#444"]} />
-      <Environment preset="sunset" />
-      <Camera />
-      <PostProcessing />
-      {/* <PerformanceMonitor /> */}
+    <ComponentRenderLogger>
+      <Canvas flat dpr={1}>
+        <color attach="background" args={["#444"]} />
+        <Environment preset="sunset" />
+        <Camera />
+        <PostProcessing />
+        {/* <PerformanceMonitor /> */}
 
-      <Gameplay />
-    </Canvas>
+        <Gameplay />
+      </Canvas>
+    </ComponentRenderLogger>
   )
 }
 
