ffd448a580b0fe813219bcf9421da783504ac134
Hendrik Mans <hendrik@mans.de>
2022-07-26 17:00:38 +0200

    Bounce ball off walls

--- src/scenes/Gameplay/MainLoop.tsx
+++ src/scenes/Gameplay/MainLoop.tsx
@@ -1,5 +1,11 @@
 import { useFrame } from "@react-three/fiber"
-import { useGameplayStore } from "./state"
+import { courtHeight, ballRadius, courtWidth } from "./configuration"
+import {
+  increaseEnemyScore,
+  increasePlayerScore,
+  setIntensity,
+  useGameplayStore
+} from "./state"
 
 export const MainLoop = () => {
   const { ball, ballDirection, ballSpeed } = useGameplayStore()
@@ -7,9 +13,38 @@ export const MainLoop = () => {
   useFrame((_, dt) => {
     if (!ball) return
 
-    /* Move ball */
-    ball.position.x += ballDirection.x * ballSpeed * dt
-    ball.position.y += ballDirection.y * ballSpeed * dt
+    {
+      /* Move ball */
+      ball.position.x += ballDirection.x * ballSpeed * dt
+      ball.position.y += ballDirection.y * ballSpeed * dt
+    }
+
+    {
+      const verticalRange = courtHeight / 2 - ballRadius
+      const horizontalRange = courtWidth / 2 - ballRadius
+
+      /* Collision with upper bounds - just bounce off the wall */
+      if (ball.position.y < -verticalRange) {
+        ballDirection.y *= -1
+        ball.position.y = -verticalRange
+      } else if (ball.position.y > verticalRange) {
+        ballDirection.y *= -1
+        ball.position.y = verticalRange
+      }
+
+      /* Horizontal collision with wall -- score! */
+      if (ball.position.x < -horizontalRange) {
+        ballDirection.x *= -1
+        ball.position.x = -horizontalRange
+        increaseEnemyScore()
+        setIntensity(1)
+      } else if (ball.position.x > horizontalRange) {
+        ballDirection.x *= -1
+        ball.position.x = horizontalRange
+        increasePlayerScore()
+        setIntensity(1)
+      }
+    }
   })
 
   return null
