bff675a186dd4095079b51239b9ab444c02a240b
Hendrik Mans <hendrik@mans.de>
2022-07-26 22:40:53 +0200

    Make it more annoying

--- src/scenes/Gameplay/Court.tsx
+++ src/scenes/Gameplay/Court.tsx
@@ -1,6 +1,25 @@
 import { GroupProps, MeshProps } from "@react-three/fiber"
+import { ColorDepthEffect } from "postprocessing"
 import { forwardRef } from "react"
-import { Mesh } from "three"
+import {
+  ShaderMaterialMaster,
+  pipe,
+  VertexPosition,
+  Mul,
+  Round,
+  Add,
+  Time,
+  Fract,
+  SplitVector3,
+  Smoothstep,
+  Sub,
+  vec3,
+  Sin,
+  Cos
+} from "shader-composer"
+import { useShader } from "shader-composer-r3f"
+import { Simplex3DNoise } from "shader-composer-toybox"
+import { Color, Mesh } from "three"
 import { courtHeight, courtWidth } from "./configuration"
 import { setGameObject } from "./state"
 
@@ -21,17 +40,36 @@ const VerticalWall = forwardRef<Mesh, MeshProps>((props, ref) => (
 ))
 
 const MiddleLine = () => (
-  <mesh>
-    <planeGeometry args={[0.05, courtHeight]} />
+  <mesh position-z={0.001}>
+    <planeGeometry args={[0.3, courtHeight]} />
     <meshStandardMaterial color={COLOR} opacity={0.2} transparent />
   </mesh>
 )
 
 const Background = () => {
+  const shader = useShader(() => {
+    const time = Time()
+    const position = Add(VertexPosition, vec3(Mul(time, 2), time, 0))
+    const [x, y, z] = SplitVector3(position)
+    const fx = Fract(x)
+    const fy = Fract(y)
+
+    const sx = Smoothstep(0, 0.05, fx)
+    const sy = Smoothstep(0, 0.05, fy)
+
+    const a = Mul(sx, sy)
+
+    return ShaderMaterialMaster({
+      color: new Color("hotpink"),
+      alpha: Sub(0.52, Mul(a, 0.5))
+    })
+  }, [])
+
   return (
     <mesh>
       <planeGeometry args={[courtWidth, courtHeight]} />
-      <meshStandardMaterial color={COLOR} opacity={0.015} transparent />
+      <shaderMaterial {...shader} transparent key={Math.random()} />
+      {/* <meshStandardMaterial color={COLOR} opacity={0.015} transparent /> */}
     </mesh>
   )
 }
